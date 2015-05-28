55f63dbeabb342a8013f8bab4fc5bb06ef7db6aa
Faerul Salamun <faerulsalamun@gmail.com>
2015-05-29 03:37:41 +0700

    fix config

--- config/app.js
+++ config/app.js
@@ -3,9 +3,9 @@ var appName = 'Sample';
 
 var port = process.env.PORT ? process.env.PORT : 3000;
 var webPrefix = '';
-var staticHost = process.env.OPENSHIFT_NODEJS_PORT + process.env.OPENSHIFT_NODEJS_IP;
-var serverIp = process.env.OPENSHIFT_NODEJS_PORT + process.env.OPENSHIFT_NODEJS_IP;
-var serverPath = process.env.OPENSHIFT_NODEJS_PORT + process.env.OPENSHIFT_NODEJS_IP;
+var staticHost = process.env.OPENSHIFT_NODEJS_IP + process.env.OPENSHIFT_NODEJS_PORT;
+var serverIp = process.env.OPENSHIFT_NODEJS_IP + process.env.OPENSHIFT_NODEJS_PORT;
+var serverPath = process.env.OPENSHIFT_NODEJS_IP + process.env.OPENSHIFT_NODEJS_PORT;
 var urlWeb = '';
 var ip =  process.env.IP || '127.0.0.1';
 
